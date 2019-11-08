da69b2da2ba694dc5f8a0f741bf663270c8dd306
Cindy <cchaprak@gmail.com>
2019-11-08 16:55:25 -0500

    user input, axios call

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios")
+
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your Github name?",
+    name: "name"
+  },
+  {
+    type: "list",
+    message: "What is your favorite color? ",
+    choices:["Blue","Red","White","Black"],
+    name: "preferredComm"
+  }
+
+
+]).then(function(response){
+axios.get("https://api.github.com/users/" + response.name )
+   .then(function(axiosResponse){
+       console.log(axiosResponse)
+   })
+})
